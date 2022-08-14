9489ebaed821d0a920d6cc0662e49b67226a3cea
Crypt00o <octopus2672001@gmail.com>
2022-08-14 15:31:06 -0400

    update .circle ci/conifg.yaml to deploy

--- udagram/udagram-frontend/src/environments/environment.ts
+++ udagram/udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:3000/api/v0'
+  apiHost: 'http://api-udagram-dev.eba-bgzp4gpr.us-east-1.elasticbeanstalk.com/api/v0'
 };
 
 /*
